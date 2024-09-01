3a3e03d8948b68f19cec72247a9b254abf98e473
abdullahalrafi <abdullah.rafi@bracits.com>
2024-09-01 22:06:13 +0600

    created the project

---
+++ src/test/java/com/example/online_book_sharing_system_v1/OnlineBookSharingSystemV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_book_sharing_system_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookSharingSystemV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
